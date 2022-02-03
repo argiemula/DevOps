d8be2de63abe763fd66a193245b726cc2ea46eba
Argie <sd.argie.mula.com>
2022-02-03 21:37:52 +0800

    Our First Commit

---
+++ src/test/java/com/societydeveloper/deploy/DeployHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.societydeveloper.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
